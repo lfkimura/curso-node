1b25fe3f5c8ad56c0b27e87d85d89934fca8b2af
U-GRUPOAMIL\lukimura <lukimura@amldskt09fpdz.grupoamil.com.br>
2016-11-14 17:31:01 -0200

    aula 3

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -12,7 +12,18 @@ app.get('/produtos/form', function (req,res){
 	console.log('cheguei aqui');
 	res.render('produtos/form');
 	});
+
+app.post('produtos/salva', function(req,res) {
+	console.log("posting form");
+	var connection = app.infra.connectionFactory();
+	var produtosBanco = new app.infra.ProdutoDAO(connection);
+	produtosBanco.salva(function(err,results) {
+		res.render('produtos/lista', { lista: results});
+	});
+	});
+
 }
+
 
 module.exports = function (app){
 	return produtos(app);
