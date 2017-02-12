22c676eb1d488aa8492923ec897b3e2506fe8bd5
Luis Kimura <Luis Kimura>
2017-02-12 20:21:20 -0200

    testes

--- produtosWeb.js
+++ produtosWeb.js
@@ -1,6 +1,9 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){ 
+	console.log(req.url)
+	console.log(req.headers)
+	console.log(res.status)
 	res.end("<html><body><h1>Listando os produtos</h1></body></html>"); 
 	})
 server.listen(3000);
